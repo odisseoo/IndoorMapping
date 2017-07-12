981687d1e4b8227e3c30b718bc61d3d59dca59d9
odisseo <lorenzo.colombo8@studenti.unimi.it>
2017-07-12 14:05:14 +0200

    added buttons to change command mod. added label editing. added shown id for every node

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ var FRAME_INTERVAL=30
 var nextId=0
 var ptrSelectedNode;
 var selected
-var removeButton;
+var submitRename;
+var inputLabel;
+var showLabel=-1; //show labels of nodes
 
 var commandCode="InsertNode"
 
 var canvas = document.getElementById("maincanvas")
 var ctx = canvas.getContext("2d")
+ctx.font="20px Georgia";
+
+
 function sub(){
 	pic.src=document.getElementById("picField").value;
 	running=setInterval(update,FRAME_INTERVAL);
@@ -40,15 +45,7 @@ document.addEventListener('keydown', function(event) {
     else if(event.keyCode == 38) {
         cameray-=20
     }
-    else if(event.keyCode == 83) { 		// s
-		commandCode="SelectNode"
-		updateCommandInfo();
-    }
-    else if(event.keyCode == 73) { 		// s
-		commandCode="InsertNode"
 
-		updateCommandInfo();
-    }
     console.log(event.keyCode)
 },false);
 
@@ -71,16 +68,22 @@ function Node(idd,string,ax,ay){
 		if(this.active==true){
 			console.log("should be red")
 			this.setInfoPanel()
-			ctx.fillStyle="#FF0000"
+			ctx.fillStyle="#00aa00"
 		}
 		else
-			ctx.fillStyle="#000000"
+			ctx.fillStyle="#808080"
 		ctx.fill();
+		ctx.fillStyle="#000000"
+		ctx.fillText(this.id+" ",this.x-camerax-5,this.y-cameray+5)
+		if(showLabel==1){
+			ctx.fillText(this.label,this.x-camerax+22,this.y-cameray)
+
+		}
 	}
 }
 
 function addNode(x,y){
-	n = new Node(nextId,"label",x+camerax,y+cameray)
+	n = new Node(nextId,"default label",x+camerax,y+cameray)
 	changeSelected(n)
 	nodeArray.push(n)
 	nextId++;
@@ -145,28 +148,74 @@ function dist(ax,ay,bx,by){
 }
 
 function updateCommandInfo(){
-	document.getElementById("commandMode").innerHTML="Command Mode: "+commandCode
-	if(commandCode=="SelectNode")
-		document.getElementById("commandMode").appendChild(removeButton)
-	else{
-		var myNode = document.getElementById("commandMode");
-		while (myNode.firstChild) {
-		    myNode.removeChild(myNode.firstChild);
+	actionNode = document.getElementById("actions")
+	if(commandCode=="SelectNode"){
+		actionNode.innerHTML=" Rename Node "
+		actionNode.appendChild(inputLabel)
+		actionNode.appendChild(submitRename)
+		actionNode.appendChild(submitRename)
+
+	}
+	else if(commandCode=="InsertNode"){
+		actionNode.innerHTML=" Click to Insert Node"
+		while (actionNode.secondChild) {
+		    actionNode.removeChild(myNode.secondChild);
 		}
 	}
 
 }
 
+function switchLabels(){
+	showLabel*=-1
+	if(showLabel>0)
+		document.getElementById("buttonShowLabel").style="background-color: #00dd00;"
+	else
+		document.getElementById("buttonShowLabel").style="background-color: #ee0000;"
+
+}
+
 initButtons()
 function initButtons(){
-	removeButton = document.createElement("button");
-	removeButton.style="margin-left:15px;"
-	removeButton.innerHTML = "Remove"
-	removeButton.onclick = function() { 
+
+  	submitRename = document.createElement("button");
+	submitRename.style="margin-left:15px;"
+	submitRename.innerHTML = "Remove"
+	submitRename.onclick = function() { 
+		if(selected){
+			selected.label=inputLabel.value
+			inputLabel.value=" "
+		}
+  	};
+  	submitRename.innerHTML="Submit label"
+  	inputLabel = document.createElement("input")
+  	inputLabel.type="text"
+  	inputLabel.style="padding-left:10px;"
+  	if(selected)
+  		inputLabel.value = selected.label
+	
+}
+
+function removeSelectedNode(){
 		index = nodeArray.indexOf(selected)
 		if(index>-1)
 			nodeArray.splice(index,1)
 		selected=null
-  };
-	
+}
+
+function setMode(calee,arg){
+	commandCode=arg
+	updateCommandInfo();
+
+	otherButtons = document.getElementById("modePanel").children;
+	for(i=0; i<otherButtons.length;i++)
+		otherButtons[i].style="background-color: initial;"
+	calee.style = "background-color: #00dd00;"
+	/*console.log(calee.id)
+	document.getElementById(calee.id).style="background-color: #000000;"
+	console.log(calee.style)
+	if(arg=="InsertNode"){
+		document.getElementById("buttonInsert").style = "background-color: #00dd00;"
+	}
+	else if(arg=="SelectNode")
+		document.getElementById("buttonSelect").style= "background-color: #00dd00;" */
 }
